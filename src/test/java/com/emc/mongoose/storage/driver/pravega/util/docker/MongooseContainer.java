88ad476a03c3a25803c3b0632511c72f2794874f
Vladislav Kulakovsky <kulakovsky.vladislav@gmail.com>
2018-11-09 03:23:18 +0300

    Add MongooseContainer like in hdfs storage driver. Add ContainerOutputCallback.java from hdfs driver
    Developer's Certificate of Origin 1.1
    
    By making a contribution to this project, I certify that:
    
    (a) The contribution was created in whole or in part by me and I have the right to submit it under the open source license indicated in the file; or
    
    (b) The contribution is based upon previous work that, to the best of my knowledge, is covered under an appropriate open source license and I have the right under that license to submit that work with modifications, whether created in whole or in part by me, under the same open source license (unless I am permitted to submit under a different license), as indicated in the file; or
    
    (c) The contribution was provided directly to me by some other person who certified (a), (b) or (c) and I have not modified it.
    
    (d) I understand and agree that this project and the contribution are public and that a record of the contribution (including all personal information I submit with it, including my sign-off) is maintained indefinitely and may be redistributed consistent with this project or the open source license(s) involved.
    
    "Signed-off-by: Vladislav Kulakovsky kulakovsky.vladislav@gmail.com"

---
+++ src/test/java/com/emc/mongoose/storage/driver/pravega/util/docker/MongooseContainer.java
@@ -0,0 +1,206 @@
+package com.emc.mongoose.storage.driver.pravega.util.docker;
+
+import com.emc.mongoose.config.BundledDefaultsProvider;
+import com.github.akurilov.confuse.Config;
+import com.github.akurilov.confuse.SchemaProvider;
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.async.ResultCallback;
+import com.github.dockerjava.api.command.CreateContainerResponse;
+import com.github.dockerjava.api.model.Bind;
+import com.github.dockerjava.api.model.ExposedPort;
+import com.github.dockerjava.api.model.Frame;
+import com.github.dockerjava.api.model.Volume;
+import com.github.dockerjava.core.DockerClientBuilder;
+import com.github.dockerjava.core.command.BuildImageResultCallback;
+import com.github.dockerjava.core.command.WaitContainerResultCallback;
+
+import java.io.Closeable;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
+
+import static com.emc.mongoose.Constants.APP_NAME;
+import static com.emc.mongoose.Constants.USER_HOME;
+import static com.emc.mongoose.config.CliArgUtil.ARG_PATH_SEP;
+
+public class MongooseContainer
+		implements Runnable, Closeable {
+
+	private static final Logger LOG = Logger.getLogger(MongooseContainer.class.getSimpleName());
+	private static final String BASE_DIR = new File("").getAbsolutePath();
+	private static final String APP_VERSION;
+
+	static {
+		final Config bundleDefaults;
+		try {
+			final Map<String, Object> schema = SchemaProvider.resolveAndReduce(
+					APP_NAME, Thread.currentThread().getContextClassLoader()
+			);
+			bundleDefaults = new BundledDefaultsProvider().config(ARG_PATH_SEP, schema);
+		} catch (final Exception e) {
+			throw new IllegalStateException(
+					"Failed to load the bundled default config from the resources", e
+			);
+		}
+		APP_VERSION = bundleDefaults.stringVal("run-version");
+	}
+
+	private static final String MONGOOSE_DIR = Paths.get(USER_HOME, "." + APP_NAME, APP_VERSION).toString();
+	public static final String CONTAINER_SHARE_PATH = "/root/.mongoose/" + APP_VERSION + "/share";
+	public static final Path HOST_SHARE_PATH = Paths.get(MONGOOSE_DIR, "log");
+
+	static {
+		HOST_SHARE_PATH.toFile().mkdir();
+	}
+
+	private static final String CONTAINER_LOG_PATH = "/root/.mongoose/" + APP_VERSION + "/log";
+	public static final Path HOST_LOG_PATH = Paths.get(MONGOOSE_DIR, "log");
+
+	static {
+		HOST_LOG_PATH.toFile().mkdir();
+	}
+
+	private final List<String> configArgs = new ArrayList<>();
+	private final StringBuilder stdOutBuff = new StringBuilder();
+	private final StringBuilder stdErrBuff = new StringBuilder();
+	private final ResultCallback<Frame> streamsCallback = new ContainerOutputCallback(
+			stdOutBuff, stdErrBuff
+	);
+	private final DockerClient dockerClient;
+	private final int durationLimitSeconds;
+	private String testContainerId = null;
+	private long duration = -1;
+	private String stdOutput = null;
+	private int containerExitCode = -1;
+
+	public long getDuration() {
+		return duration;
+	}
+
+	public String getStdOutput() {
+		return stdOutput;
+	}
+
+	private int getContainerExitCode() {
+		return containerExitCode;
+	}
+
+	public MongooseContainer(final List<String> configArgs, final int durationLimitSeconds) {
+		this.durationLimitSeconds = durationLimitSeconds;
+		this.dockerClient = DockerClientBuilder.getInstance().build();
+		final File dockerBuildFile = Paths
+				.get(BASE_DIR, "docker", "Dockerfile")
+				.toFile();
+		LOG.info("Build mongoose image w/ Pravega support using the dockerfile " + dockerBuildFile);
+		final BuildImageResultCallback buildImageResultCallback = new BuildImageResultCallback();
+		this.dockerClient
+				.buildImageCmd()
+				.withBaseDirectory(new File(BASE_DIR))
+				.withDockerfile(dockerBuildFile)
+				.withBuildArg("MONGOOSE_VERSION", APP_VERSION)
+				.withPull(true)
+				.withTags(Collections.singleton("emcmongoose/mongoose-storage-driver-pravega:testing"))
+				.exec(buildImageResultCallback);
+		final String testingImageId = buildImageResultCallback.awaitImageId();
+		LOG.info("Build mongoose testing image id: " + testingImageId);
+		this.configArgs.add("--output-metrics-trace-persist=true");
+		this.configArgs.add("--storage-net-node-port=" + PravegaNodeContainer.PORT);
+		for (final String configArg : configArgs) {
+			if (configArg.startsWith("--run-scenario=")) {
+				final String scenarioPathStr = configArg.substring("--run-scenario=".length());
+				if (scenarioPathStr.startsWith(HOST_SHARE_PATH.toString())) {
+					this.configArgs.add(
+							"--run-scenario=" + CONTAINER_SHARE_PATH
+									+ scenarioPathStr.substring(HOST_SHARE_PATH.toString().length())
+					);
+				} else {
+					this.configArgs.add(
+							"--run-scenario=" + CONTAINER_SHARE_PATH + scenarioPathStr
+					);
+				}
+			} else {
+				this.configArgs.add(configArg);
+			}
+		}
+		LOG.info("Mongoose test container arguments: " + Arrays.toString(configArgs.toArray()));
+		final Volume volumeShare = new Volume(CONTAINER_SHARE_PATH);
+		final Volume volumeLog = new Volume(CONTAINER_LOG_PATH);
+		final Bind[] binds = new Bind[]{
+				new Bind(HOST_SHARE_PATH.toString(), volumeShare),
+				new Bind(HOST_LOG_PATH.toString(), volumeLog),
+		};
+		// put the environment variables into the container
+		final Map<String, String> envMap = System.getenv();
+		final String[] env = envMap.keySet().toArray(new String[envMap.size()]);
+		for (int i = 0; i < env.length; i++) {
+			if ("PATH".equals(env[i])) {
+				env[i] = env[i] + "=" + envMap.get(env[i]) + ":/bin";
+			} else {
+				env[i] = env[i] + "=" + envMap.get(env[i]);
+			}
+		}
+		final CreateContainerResponse container = dockerClient
+				.createContainerCmd(testingImageId)
+				.withName("mongoose")
+				.withNetworkMode("host")
+				.withExposedPorts(ExposedPort.tcp(9010), ExposedPort.tcp(5005))
+				.withVolumes(volumeShare, volumeLog)
+				.withBinds(binds)
+				.withAttachStdout(true)
+				.withAttachStderr(true)
+				.withEntrypoint("/opt/mongoose/entrypoint-storage-driver-pravega.sh")
+				.withEnv(env)
+				.withCmd(this.configArgs)
+				.exec();
+		testContainerId = container.getId();
+		LOG.info("Created the mongoose test container w/ id: " + testContainerId);
+	}
+
+	public final void clearLogs(final String stepId) {
+		final File logDir = Paths.get(HOST_LOG_PATH.toString(), stepId).toFile();
+		final File[] logDirFiles = logDir.listFiles();
+		if (logDirFiles != null) {
+			for (final File logFile : logDirFiles) {
+				logFile.delete();
+			}
+		}
+		logDir.delete();
+	}
+
+	@Override
+	public final void run() {
+		dockerClient
+				.attachContainerCmd(testContainerId)
+				.withStdErr(true)
+				.withStdOut(true)
+				.withFollowStream(true)
+				.exec(streamsCallback);
+		duration = System.currentTimeMillis();
+		dockerClient.startContainerCmd(testContainerId).exec();
+		containerExitCode = dockerClient
+				.waitContainerCmd(testContainerId)
+				.exec(new WaitContainerResultCallback())
+				.awaitStatusCode(durationLimitSeconds, TimeUnit.SECONDS);
+		duration = System.currentTimeMillis() - duration;
+		stdOutput = stdOutBuff.toString();
+	}
+
+	@Override
+	public final void close()
+			throws IOException {
+		streamsCallback.close();
+		if (testContainerId != null) {
+			dockerClient
+					.removeContainerCmd(testContainerId)
+					.withForce(true)
+					.exec();
+			testContainerId = null;
+		}
+		dockerClient.close();
+	}
+}
