d38fbb5d8fa2b5b69609bf4dfedd3ed7cd1f8dc8
Andrey Kurilov <andrey.kurilov@emc.com>
2019-03-23 14:24:50 +0300

    more testing under CI env

--- src/test/java/com/emc/mongoose/storage/driver/pravega/util/PravegaNode.java
+++ src/test/java/com/emc/mongoose/storage/driver/pravega/util/PravegaNode.java
@@ -7,8 +7,10 @@ public interface PravegaNode {
 	static String addr() {
 		final boolean ciFlag = null != System.getenv("CI");
 		if(ciFlag) {
-			return System.getenv("SERVICE_HOST");
+			System.out.println("Running under CI, using \"storage\" host for the testing");
+			return "storage";
 		} else {
+			System.out.println("Running under CI, using \"localhost\" for the testing");
 			return "localhost";
 		}
 	}
