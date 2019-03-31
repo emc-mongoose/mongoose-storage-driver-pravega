fdc90503d50b0bf3c9f373a676f0cd9e179338e6
Andrey Kurilov <andrey.kurilov@emc.com>
2019-03-31 17:23:51 +0300

    try

--- src/test/java/com/emc/mongoose/storage/driver/pravega/util/PravegaNode.java
+++ src/test/java/com/emc/mongoose/storage/driver/pravega/util/PravegaNode.java
@@ -7,7 +7,7 @@ public interface PravegaNode {
 	static String addr() {
 		final boolean ciFlag = null != System.getenv("CI");
 		if(ciFlag) {
-			return System.getenv("pravega");
+			return "pravega";
 		} else {
 			return "localhost";
 		}
