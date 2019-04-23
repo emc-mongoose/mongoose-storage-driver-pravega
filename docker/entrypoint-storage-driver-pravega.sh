e731fbec7e1b14d2f0eae731b3a366508ff5bffb
Kurilov, Andrey <andrey.kurilov@emc.com>
2019-04-23 10:22:24 +0300

    try

--- docker/entrypoint-storage-driver-pravega.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-umask 0000
-java -jar /opt/mongoose/mongoose.jar --storage-driver-type=pravega --storage-net-node-port=9090 "$@"
--- getPravegaVersion.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-./gradlew properties --no-daemon --console=plain -q | grep "^version:" | awk '{printf $2}'
