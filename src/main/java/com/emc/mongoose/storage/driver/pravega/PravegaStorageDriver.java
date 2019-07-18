58c0e699159a142dee131453688da35f7fcd5979
kurila <akurilov123@gmail.com>
2019-07-18 15:22:11 +0300

    v4.2.20

--- src/main/java/com/emc/mongoose/storage/driver/pravega/PravegaStorageDriver.java
+++ src/main/java/com/emc/mongoose/storage/driver/pravega/PravegaStorageDriver.java
@@ -140,7 +140,6 @@ public class PravegaStorageDriver<I extends DataItem, O extends DataOperation<I>
 	private final Lock lastFailedStreamPosLock = new ReentrantLock();
 	private volatile AsyncIterator<Stream> streamIterator = null;
 	private final boolean controlScopeFlag;
-	private final URLClassLoader extClsLoader;
 	private final Credentials cred;
 
 	@Value
@@ -340,11 +339,8 @@ public class PravegaStorageDriver<I extends DataItem, O extends DataOperation<I>
 		this.bgExecutor = Executors.newScheduledThreadPool(
 						Runtime.getRuntime().availableProcessors(),
 						new LogContextThreadFactory(toString(), true));
-		val res = new CoreResourcesToInstall();
-		val appHomePath = res.appHomePath();
-		extClsLoader = Extension.extClassLoader(Paths.get(appHomePath.toString(), DIR_EXT).toFile());
 		cred = ServiceLoader
-			.load(Credentials.class, extClsLoader)
+			.load(Credentials.class, Credentials.class.getClassLoader())
 			.findFirst()
 			.orElseGet(this::getBasicCredentialsIfAny);
 		Loggers.MSG.info("Authentication type: {}", cred == null ? "none" : cred.getAuthenticationType());
@@ -1169,7 +1165,6 @@ public class PravegaStorageDriver<I extends DataItem, O extends DataOperation<I>
 	protected void doClose()
 					throws IOException {
 		super.doClose();
-		extClsLoader.close();
 		// clear all caches & pools
 		evtReaderGroupManagerCreateFuncCache.clear();
 		closeAllWithTimeout(evtReaderGroupManagerCache.values());
