13487cbae8ade28132572f905de6a9ed734fc78c
Alexander Aphonin <Alexander.Aphonin@emc.com>
2020-01-20 20:17:38 +0300

    Try avoiding using an env var in test

--- src/test/robot/api/storage/data/read_all_byte_streams.js
+++ src/test/robot/api/storage/data/read_all_byte_streams.js
@@ -1,24 +1,11 @@
-var sharedConfig = {
-	"storage": {
-		"driver": {
-			"stream": {
-				"data": "bytes"
-			}
-		},
-		"namespace": SCOPE_NAME
-	}
-}
-
 PreconditionLoad
-	.config(sharedConfig)
 	.run();
 
 ReadLoad
-	.config(sharedConfig)
 	.config({
 		"item" : {
 			"input" : {
-				"path" : SCOPE_NAME
+				"path" : "scope4"
 			}
 		}
 	})
